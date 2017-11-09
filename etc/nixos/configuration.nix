3a0c4dea1dc6e637c1ae50386f55676c25ac774b
Xavier Maso <xav.maso@gmail.com>
2017-11-09 22:54:50 +0100

    Correct grub related content
      * most of the mapping is done in `hardware-configuration.nix`
      (which is generated)
      * need to specify device

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -9,16 +9,12 @@
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
   # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.enableCryptodisk = true;
-  boot.initrd.luks.devices.crypted.device = "/dev/sda1";
-  fileSystems."/".device = "/dev/mapper/crypted";
-
-  swapDevices = [ { device = "/dev/disk/by-label/swap"; } ];
 
   networking.hostName = "w"; # Define your hostname.
   networking.networkmanager.enable = true;
