600df72c678cb815aaf3a96afe1acde70b79b4ea
Pamplemousse <xav.maso@gmail.com>
2018-01-04 10:23:24 +0100

    Clean `etc/nixos/configuration.nix`
      * alphabetical order whenever possible
      * remove redondent comments

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -11,19 +11,6 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.enableCryptodisk = true;
 
-  networking.hostName = "w"; # Define your hostname.
-  networking.networkmanager.enable = true;
-
-  # Select internationalisation properties.
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "fr-bepo";
-    defaultLocale = "en_US.UTF-8";
-  };
-
-  # Set your time zone.
-  time.timeZone = "Europe/Amsterdam";
-
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
@@ -46,37 +33,42 @@
     vlc
   ];
 
-  virtualisation.docker.enable = true;
-  virtualisation.docker.enableOnBoot = true;
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "fr-bepo";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  networking.hostName = "w";
+  networking.networkmanager.enable = true;
 
   services.logind.extraConfig = "
     HandleLidSwitch=suspend
     HandleLidSwitchDocked=suspend
   ";
 
-  # Enable the X11 windowing system.
   services.xserver = {
+    desktopManager.gnome3.enable = true;
+    displayManager.gdm.enable = true;
     enable = true;
     layout = "fr";
     xkbVariant = "bepo";
   };
 
-  # services.xserver.xkbOptions = "eurosign:e";
+  system.stateVersion = "17.09";
+  system.autoUpgrade.enable = true;
 
-  # Enable the Gnome Desktop Environment.
-  services.xserver.desktopManager.gnome3.enable = true;
-  services.xserver.desktopManager.gdm.enable = true;
+  time.timeZone = "Europe/Amsterdam";
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.pamplemousse = {
-    isNormalUser = true;
     description = "Xavier Maso";
     extraGroups = [ "wheel" "networkmanager" "docker" ];
     home = "/home/pamplemousse";
+    isNormalUser = true;
     shell = pkgs.zsh;
     uid = 1000;
   };
 
-  # The NixOS release to be compatible with for stateful data such as databases.
-  system.stateVersion = "17.03";
+  virtualisation.docker.enable = true;
+  virtualisation.docker.enableOnBoot = true;
 }
