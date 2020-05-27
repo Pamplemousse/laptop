e29c6955d57f3d8d0d5da14dbf15802ebdfc8158
Pamplemousse <xav.maso@gmail.com>
2020-05-26 18:28:01 -0700

    Apparently, LUKS header are bigger now
    https://askubuntu.com/questions/706585/cryptsetup-cannot-wipe-header-on-device#answer-1185737
    
    And `/dev/urandom` is non-blocking...
    https://www.2uo.de/myths-about-urandom/

--- install.sh
+++ install.sh
@@ -6,19 +6,21 @@
 fdisk -l
 read -r -p "Which disk shall we install everything on? (eg: /dev/sda) " DISK
 RAM="$(free --giga | awk 'NR==2{print $2}')"
+# 16MiB for header, 4MiB for the keyfile (random data)
+CRYPTKEY_PARTITION_SIZE="20MiB"
 
 # format and create partitions
 sgdisk -og "$DISK"
 sgdisk -n 1:2048:4095 -c 1:"BIOS boot partition" -t 1:ef02 "$DISK"
 sgdisk -n 2:0:+550MiB -c 2:"EFI system partition" -t 2:ef00 "$DISK"
-sgdisk -n 3:0:+3MiB -c 3:"cryptsetup luks key" -t 3:8300 "$DISK"
+sgdisk -n 3:0:+"${CRYPTKEY_PARTITION_SIZE}" -c 3:"cryptsetup luks key" -t 3:8300 "$DISK"
 sgdisk -n 4:0:+"${RAM}"GiB -c 4:"swap space (hibernation)" -t 4:8300 "$DISK"
 sgdisk -n 5:0:"$(sgdisk -E "$DISK")" -c 5:"root filesystem" -t 5:8300 "$DISK"
 
 cryptsetup luksFormat "${DISK}3"
 cryptsetup luksOpen "${DISK}3" cryptkey
 
-dd if=/dev/random of=/dev/mapper/cryptkey bs=1024 count=14000
+dd if=/dev/urandom of=/dev/null bs=1KB count=4MB iflag=count_bytes,fullblock
 
 cryptsetup luksFormat --key-file=/dev/mapper/cryptkey "${DISK}4"
 
