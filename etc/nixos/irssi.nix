7c696b904cb9966fd4892497691ae18294d5c822
Pamplemousse <xav.maso@gmail.com>
2020-05-29 05:57:29 +0200

    I don't use hangouts on IRSSI anymore

--- etc/nixos/irssi.nix
+++ etc/nixos/irssi.nix
@@ -2,20 +2,9 @@
 
 {
   environment.systemPackages = with pkgs; [
-    bitlbee
     irssi
     perlPackages.LWP
     perlPackages.LWPProtocolHttps
     perlPackages.NetSSLeay
-    purple-hangouts
   ];
-
-  nixpkgs.config.bitlbee.enableLibPurple = true;
-
-  services = {
-    bitlbee = {
-      enable = true;
-      libpurple_plugins = [ pkgs.purple-hangouts ];
-    };
-  };
 }
