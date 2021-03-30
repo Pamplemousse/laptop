0296f2f4c25cc31388052e07248cc0bc8f1c54f2
Pamplemousse <xav.maso@gmail.com>
2021-03-30 11:19:12 -0700

    Make installation lighter for GH Actions
    Signed-off-by: Pamplemousse <xav.maso@gmail.com>

--- tests/test_build.sh
+++ tests/test_build.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-# Needs to specify where to find `nixos-generate` during GitHub Actions.
-if ! nixos-generate --help >/dev/null 2>&1; then
-  if [ -n "$GITHUB_WORKSPACE" ]; then
-    export PATH=$GITHUB_WORKSPACE/nixos-generators:$PATH
-  fi
-fi
-
 # Work on a copy of the configuration, trimmed from hardware specific content.
 cp ./etc/nixos/configuration.nix ./etc/nixos/.configuration.nix
 patch ./etc/nixos/.configuration.nix tests/patches/hardwareless_configuration.patch
+
+if [ -n "$GITHUB_WORKSPACE" ]; then
+  # GitHub Actions are run with a limited amount of space.
+  patch ./etc/nixos/.configuration.nix tests/patches/lighter_configuration.patch
+
+  # Needs to specify where to find `nixos-generate` during GitHub Actions.
+  if ! nixos-generate --help >/dev/null 2>&1; then
+      export PATH=$GITHUB_WORKSPACE/nixos-generators:$PATH
+  fi
+fi
 
 nixos-generate -f vm -c ./etc/nixos/.configuration.nix
